09c79a3e9cd5ffec641620ff2c49b59593f2857d
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-09-10 09:24:31 +0430

    real_estate_ui

---
+++ lib/tools/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const backGroundColor=Color(0xffF7FBFC);
+const searchBarColor=Color(0xffe5effd);
+const primaryColor=Color(0xff55bbd9);
+const primaryColor2=Color(0xff85caff);
+const iconColor=Color(0xffb7b8e0);
